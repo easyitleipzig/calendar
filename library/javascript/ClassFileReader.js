0b304188914bd700ba14f6716eba04b27403c807
Olaf Thiele <easyit.leipzig@gmail.com>
2023-11-12 07:57:32 +0100

    Sicherungscommit

--- library/javascript/ClassFileReader.js
+++
@@ -1,8 +0,0 @@
-class FileReader {
-	constructor( param ) {
-        this.opt = {
-            id:                 "", // necessary - id of element
-        }
-        Object.assign( this.opt, param );
-
-}
